6ecc550484c8bd26a266902e83211f23bb9371df
Dozimetrist <@rr.rr>
2021-03-12 13:32:52 +0200

    Сейчас программа умеет выбирать 1 из 4 имеющихся мемчиков. Если она не понимает команду - показывает все подряд.

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -14,8 +14,122 @@ namespace MemeArchive
          * то последовательно вывести в консоль все арты с их именами */
 
 
+
+
         {
-            Console.WriteLine("Please enter the meme's name");
+            string command;
+
+            do
+            {
+
+                Console.WriteLine("Please enter the meme's name");
+                command = Console.ReadLine();
+                if (ShouldLeave(command))
+                {
+                    break;
+                }
+
+                MemeSelectorForDisplay(command);
+
+            }
+            while (true);
+
+            Console.WriteLine("Bye");
+
+        }
+
+        private static bool ShouldLeave(string command)
+        {
+            return command == "exit";
+        }
+
+        private static void MemeSelectorForDisplay(string command)
+        {
+            if (command == "bat")
+            {
+                ShowBat();
+            }
+            else if (command == "tree")
+            {
+                ShowTree();
+
+            }
+            else if (command == "man")
+            {
+                ShowMan();
+
+            }
+            else if (command == "flower")
+            {
+                ShowFlower();
+            }
+            else
+            {
+                ShowAllMemes();
+            }
+        }
+
+
+
+        private static void ShowAllMemes()
+        {
+            Console.WriteLine("Please see all the memes, pick one at a time!");
+            ShowFlower();
+            ShowMan();
+            ShowBat();
+            ShowTree();
+
+        }
+
+        private static void ShowFlower()
+        {
+            Console.WriteLine(@"
+                                _   _
+                             .-( '.' )-.
+                            (   \ : /   )
+                           ( `'-.;;;.-'` )
+                          ( :-==;;;;;==-: )
+                           (  .-';;;'-.  )
+                            (`  / : \  `)
+                             '-(_.'._)-'
+
+                            That's a flower!");
+        }
+
+        private static void ShowMan()
+        {
+            Console.WriteLine(@"
+                                (*_*)
+                               \(   )/
+                                /   \
+
+                              That's a man!");
+        }
+
+        private static void ShowTree()
+        {
+            Console.WriteLine(@"
+                                *
+                               ***
+                              *****
+
+                          That's a tree!  ");
+        }
+
+
+        private static void ShowBat()
+        {
+
+            Console.WriteLine(@"
+                                 /\                 /\
+                                / \'._   (\_/)   _.' /\
+                               / .''._'--(o.o)--'_.''. \
+                              /.' _/ |`' =/  \= '`| \_`.\
+                             /` .` `\:-,`\___ / `,-:/``.`\
+                            /.- '      `\(-V-)/`      `- .\
+                           `             ''   ''    
+`
+                                      That's a bat!");
 
         }
     }
