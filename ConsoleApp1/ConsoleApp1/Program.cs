781530ca4c326a57eafab6346cd890409d260658
Dozimetrist <@rr.rr>
2021-03-13 13:33:23 +0200

    Программа доделана согласно ТЗ. В данном коммите сделан вывод рандомного мемчика и подсказки перед началом ввода.

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -13,23 +13,25 @@ namespace MemeArchive
          * если программа не найдёт в массиве нужный арт (или если в консоль введут какой-то мусор)
          * то последовательно вывести в консоль все арты с их именами */
 
-
-
-
         {
             string command;
+            int MemeNumber = RandomNumberGenerator();  //get random number for choose the meme
+
+            ShowRandomMeme(MemeNumber); //showing random meme, and a greeting with a random meme name
+            Greeting(MemeNumber);
 
             do
             {
+                Console.WriteLine("\n Please enter the meme's name!");
 
-                Console.WriteLine("Please enter the meme's name");
                 command = Console.ReadLine();
-                if (ShouldLeave(command))
+
+                if (ShouldLeave(command))   //parse if we should exit
                 {
                     break;
                 }
 
-                MemeSelectorForDisplay(command);
+                MemeSelectorForDisplay(command);    // Parsing from input, selecting right meme and displaying it
 
             }
             while (true);
@@ -38,12 +40,58 @@ namespace MemeArchive
 
         }
 
-        private static bool ShouldLeave(string command)
+        //Below we placing random hint what can we try to input to choose the meme to be displayed
+        private static void Greeting(int MemeNumber)
+        {
+            switch (MemeNumber)
+            {
+                case 0:
+                    Console.Write("To choose a meme to dispay, you can try type 'tree'");
+                    break;
+                case 1:
+                    Console.Write("To choose a meme to dispay, you can try type 'bat'");
+                    break;
+                case 2:
+                    Console.Write("To choose a meme to dispay, you can try type 'flower'");
+                    break;
+                case 3:
+                    Console.Write("To choose a meme to dispay, you can try type 'man'");
+                    break;
+            }
+        }
+
+        private static int RandomNumberGenerator()                       // getting random int
+        {
+            Random RandomNumber = new Random();
+            int MemeNumber = RandomNumber.Next(0, 3);
+            return MemeNumber;
+        }
+
+        private static void ShowRandomMeme(int MemeNumber)              //choosing the appropriate meme
+        {
+            switch (MemeNumber)
+            {
+                case 0:
+                    ShowBat();
+                    break;
+                case 1:
+                    ShowFlower();
+                    break;
+                case 2:
+                    ShowMan();
+                    break;
+                case 3:
+                    ShowTree();
+                    break;
+            }
+        }
+
+        private static bool ShouldLeave(string command)              //test if exit = true
         {
             return command == "exit";
         }
 
-        private static void MemeSelectorForDisplay(string command)
+        private static void MemeSelectorForDisplay(string command)  //checking which meme should we display
         {
             if (command == "bat")
             {
@@ -71,9 +119,9 @@ namespace MemeArchive
 
 
 
-        private static void ShowAllMemes()
+        private static void ShowAllMemes() //displaying all memes if program can't parse input
         {
-            Console.WriteLine("Please see all the memes, pick one at a time!");
+            Console.WriteLine("\nPlease see all the memes, but pick only one at a time!");
             ShowFlower();
             ShowMan();
             ShowBat();
@@ -127,8 +175,8 @@ namespace MemeArchive
                               /.' _/ |`' =/  \= '`| \_`.\
                              /` .` `\:-,`\___ / `,-:/``.`\
                             /.- '      `\(-V-)/`      `- .\
-                           `             ''   ''    
-`
+                            `             ''   ''          `
+                                                            
                                       That's a bat!");
 
         }
