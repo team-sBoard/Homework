f9f1d571a402a02508149b5124cf979891fad544
Dozimetrist <@rr.rr>
2021-03-19 17:51:46 +0200

    initial commit

---
+++ SellingAuto's/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SellingAuto_s
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
