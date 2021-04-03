e551d35bdeacfd29ea4494af6187ba188067b80d
Dozimetrist <@rr.rr>
2021-04-03 19:50:59 +0300

    initial commit

---
+++ MemeGallery/MemeGallery/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MemeGallery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
